bef13c2fdb055ce3f2d55d5cadd9be75f36c5f9e
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-09 14:50:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
+TOKEN = os.environ.get("TOKEN", "6486584540:AAFYLW1mfCMcSwUb8OwWsDvQIbBaCAFjqcU")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "PELMVS-DWKAPE-ZKIPPM-HIIWWR-ARQ")
 REPO_BOT = os.environ.get("REPO_BOT", "https://t.me/spacecloud")
-OWNER = os.environ.get("OWNER", "")
-BOT_NAME = os.environ.get("BOT_NAME", "")
+OWNER = os.environ.get("OWNER", "6900132473")
+BOT_NAME = os.environ.get("BOT_NAME", "MAMA PAPA")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "spacecloud")
